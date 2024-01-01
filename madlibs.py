0dd82a92ed998436ab6f7785257e32ed80064424
nick amaro <nickamaro@nicks-MacBook-Pro.local>
2023-12-31 20:54:43 -0600

    finished project to run a adlib

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,9 @@
 # string concatination
+
+adj = input("Adjective:")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famouse person: ")
+madlib = f"computer programming is so {adj}! it makes me so excited all the time because \ i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
